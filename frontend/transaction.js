7f4022ad4941c50410703f76b7a7a87d8c6470d6
Sean Yeo <seanyeo89@gmail.com>
2021-11-27 01:39:21 +0800

    transaction via wid added

--- frontend/transaction.js
+++ frontend/transaction.js
@@ -58,3 +58,33 @@ function getTransactionByTID() {
         })
         .catch((error) => console.log("error", error));
 }
+function getTransactionByWID() {
+  var id = document.getElementById('paramId').value
+  fetch(`http://localhost:3000/transaction/by-wid?id=${id}`, { method: 'GET' })
+    .then((response) => response.json())
+    .then((data) => {
+      var text = `
+              <table>
+                <tr>
+                  <th>Transaction ID</th>
+                  <th>Timestamp</th>
+                  <th>Category</th>
+                  <th>Account Name</th>
+                  <th>Amount</th>
+                </tr>`
+
+      data.forEach((item) => {
+        text += `
+                    <tr>
+                      <td>${item.transaction_id}</td>
+                      <td>${item.transaction_date}</td>
+                      <td>${item.category}</td>
+                      <td>${item.account_id}</td>
+                      <td>${item.amount}</td>
+                    </tr>`
+      })
+      text += '</table>'
+      $('.mypanel').html(text)
+    })
+    .catch((error) => console.log('error', error))
+}
